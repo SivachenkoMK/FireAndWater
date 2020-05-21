3e3d6d083646a17cb280e433e6252370843f604e
nastya3333 <63013845+nastya3333@users.noreply.github.com>
2020-05-21 13:26:05 +0300

    AddMenu

---
+++ Assets/__Scripts/VolumeValue.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeValue : MonoBehaviour
+{
+
+    public AudioSource audiosrc;
+    public float musicVolume = 1f;
+  
+   
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audiosrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audiosrc.volume = musicVolume;
+    }
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+}
