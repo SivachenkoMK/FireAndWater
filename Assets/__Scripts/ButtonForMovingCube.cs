ba26c45cc52d4af66c2e1bdd34c260f81f334a86
Elly2020 <elya.soskina@gmail.com>
2020-06-01 14:10:49 +0300

    2 and 3 level with tables bugs

---
+++ Assets/__Scripts/ButtonForMovingCube.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonForMovingCube : MonoBehaviour
+{
+public GameObject movingCube;
+    void OnTriggerEnter(Collider other)
+	{
+		movingCube.GetComponent<MovingCube>().objectsOnButtons++;
+	}
+	  void OnTriggerExit(Collider other)
+	{
+		movingCube.GetComponent<MovingCube>().objectsOnButtons--;
+	}
+}
---
+++ Assets/__Scripts/MovingCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCube : MonoBehaviour
+{
+  public float objectsOnButtons;
+  
+  void Update()
+  {
+  	  if(objectsOnButtons==0)
+	  {
+	  	 this.GetComponent<Rigidbody>().isKinematic=true;
+	  }
+	  else
+	  this.GetComponent<Rigidbody>().isKinematic=false;
+  }
+}
